fc45a9f7c975bcbb2a9d829da054852ef68592cd
aa126237 <alexaanderson2002@gmail.com>
2023-07-20 09:35:28 -0700

    first commit

---
+++ src/test/java/Carbon14DatingTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Carbon14DatingTest {
+    //#1 Build a test for calculateAge();
+
+    /*
+        #2 Build a test for getRemainingAmount() in the
+        scenario the given remaining value is <= 1.
+    */
+
+   /*
+        #3 Build a test for getRemainingAmount() in the
+        scenario the given remaining value is >= 1.
+    */
+
+    // #4 Build a test for the toString() method.
+}
